ca23eebb4b22d4eb110282c9ecab863b2f2a2c9e
wenjia <dwgia027@gmail.com>
2022-12-07 13:39:54 +1100

    finish css

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/*
+-----header-----
+1.dispear when scroll up, re-appear when scroll down
+2.crown moves to the objects been pointed at and stays at the object been clicked. move back to previousely clicked object.
+3.pull down menu for language button
+*/
+
+/*
+--------Bio-------
+click arrow to show the bio content
+ */
+
+/* -----skills-----
+1.slide and display different skill cards autoly
+2.click prev and next btn to change cards
+3.click circle to choose cards
+*/
+
+/*-------projects------ 
+1.link to the pages hosted on GitHub etc.
+2.mouseenter to emphazise the description 
+*/
+
+/*---------find me--------
+    mouseenter to show more info
+*/
